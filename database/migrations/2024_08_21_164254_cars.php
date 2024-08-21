648c7bd7014147406f19ff76dd14453f046e222f
Nawaf1w3 <57842047+Nawaf1w3@users.noreply.github.com>
2024-08-21 19:05:56 +0200

    created cars table

--- database/migrations/2024_08_21_164254_cars.php
+++ database/migrations/2024_08_21_164254_cars.php
@@ -12,8 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('Cars', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->bigIncrements('id');
+            $table->string('merk'); // The manufacturer of the car
+            $table->string('model'); // The specific model of the car
+            $table->integer('year'); // The manufacturing year of the car
+            $table->string('color'); // The color of the car
+            $table->string('license_plate'); // The car's license plate number
+            $table->string('vin')->nullable(); // The Vehicle Identification Number
+            $table->decimal('daily_rate', 5, 2); // The rental rate per day in dollars
+            $table->enum('status', ['beschikbaar', 'verhuurd', 'onderhoud']);// The status of the car
+            $table->integer('kilometers'); // The current mileage of the car
+            $table->enum('fuel_type', ['petrol', 'diesel', 'electric']); // Type of fuel
+            $table->enum('transmission_type', ['handmatig', 'automatisch']); // Transmission type
+            $table->timestamps(); // Created at and updated at timestamps
         });
     }
 
