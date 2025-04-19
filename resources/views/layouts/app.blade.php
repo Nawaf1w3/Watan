6970d7ca86a56ba0eee56e5d735a20122fc922fe
Nawaf1w3 <57842047+Nawaf1w3@users.noreply.github.com>
2025-04-19 19:09:54 +0200

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,6 @@
         }
 </style>
 <head>
-{{-- hallo --}}
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
